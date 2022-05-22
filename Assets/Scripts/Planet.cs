9d11b870cf4111ebcec8a05f89772f4c92a70da9
lior1204 <lior12042000@gmail.com>
2022-05-22 21:45:02 +0300

    Planet control and setup

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    enum Controller
+    {
+        Neutral=0,
+        Player=1,
+        Enemy=2
+    }
+
+    //parameters
+    [SerializeField] private bool isStartingPlanet = false;
+
+    //state variables
+    private int strength = 0;
+    private Controller hiveController = Controller.Neutral;
+
+    //references
+
+    void Start()
+    {
+        if (isStartingPlanet)//if is players firs plamet make player control.
+        {
+            hiveController = Controller.Player;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+}
