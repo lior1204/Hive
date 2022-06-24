05fb735edb6968141c160e193fb6ec257fbfe819
lior1204 <lior12042000@gmail.com>
2022-06-24 13:58:41 +0300

    Line and Fix

--- Assets/Scripts/Link.cs
+++ Assets/Scripts/Link.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(EdgeCollider2D))]
 public abstract class Link : MouseInteractable
 {
     [SerializeField] private float startWidth = 0.25f;
@@ -14,15 +13,12 @@ public abstract class Link : MouseInteractable
     public Planet Target { get; protected set; }
     public float TimeStemp { get; set; }
     //references
-    private EdgeCollider2D edgeCollider;
-    private LineRenderer myLine;
+    private EdgeCollider2D _edgeCollider;
+    //private LineRenderer myLine;
+    private SpriteRenderer _spriteRenderer;
     public HiveController.Hive HiveType { get { return Origin ? Origin.HiveType : HiveController.Hive.Neutral; } }
-    void Update()
-    {
-        SetLinePosition();
-        SetEdgeCollider();
-    }
-    protected static void NewLink(Link link,Planet origin, Planet target)
+
+    protected static void NewLink(Link link, Planet origin, Planet target)
     {
         if (link)
         {
@@ -31,71 +27,104 @@ public abstract class Link : MouseInteractable
             link.Target = target;
             link.isActive = false;
             link.TimeStemp = Time.time;
-            link.transform.parent = origin.transform.parent;
-            link.transform.position = Vector3.zero;
-            link.SetLine();
+            link.transform.parent = origin.transform;
+            link.transform.localPosition = Vector3.zero;
+            link._spriteRenderer = link.GetComponent<SpriteRenderer>();
+            link._edgeCollider = link.GetComponent<EdgeCollider2D>();
+            //link.SetLine();
         }
     }
+
+    void Update()
+    {
+        //SetLinePosition();
+        SetLineTransform();
+        SetEdgeCollider();
+    }
+    private void SetLineTransform()
+    {
+        float length = Vector2.Distance(Origin.transform.position, Target.transform.position);
+        _spriteRenderer.size = new Vector2(length, _spriteRenderer.size.y);
+        transform.eulerAngles = Vector3.forward * Vector2.Angle(Origin.transform.position, Target.transform.position);
+        Debug.Log("Origin: " + Origin.transform.position + " Target: " + Target.transform.position + " Angel: " + Vector2.Angle(Origin.transform.position, Target.transform.position));
+    }
+
     private void SetEdgeCollider()
     {
-        List<Vector2> edges = new List<Vector2>();
+        Vector2[] edges = new Vector2[2];
 
-        for (int point = 0; point < myLine.positionCount; point++)
-        {
-            Vector3 lineRendererPoint = myLine.GetPosition(point);
-            edges.Add(new Vector2(lineRendererPoint.x, lineRendererPoint.y));
-        }
-
-        edgeCollider.points = edges.ToArray();
-    }
-    private void SetLinePosition()
-    {
-        Vector3[] pos = { Origin.transform.position, Target.transform.position };//set to origin and target positions
-        //draw line behind planet
-        pos[0]-= new Vector3(0f, 0f, 0.01f);
-        pos[1]-= new Vector3(0f, 0f, 0.01f);
-        myLine.SetPositions(pos);
+        edges[0] = Origin.transform.position;
+        edges[0].x += _edgeCollider.edgeRadius;
+        edges[1] = Target.transform.position;
+        edges[1].x -= 2*_edgeCollider.edgeRadius;
+        _edgeCollider.points = edges;
     }
 
-    private void SetLine()
-    {
-        edgeCollider = this.GetComponent<EdgeCollider2D>();
-        myLine = this.GetComponent<LineRenderer>();
-        myLine.startWidth = startWidth;
-        myLine.endWidth = endWidth;
-        UpdateColorAndHighlight();
-    }
+    //private void SetLinePosition()
+    //{
+    //    Vector3[] pos = { Origin.transform.position, Target.transform.position };//set to origin and target positions
+    //    //draw line behind planet
+    //    pos[0] -= new Vector3(0f, 0f, 0.01f);
+    //    pos[1] -= new Vector3(0f, 0f, 0.01f);
+    //    myLine.SetPositions(pos);
+    //}
+    //private void SetLine()
+    //{
+    //    _edgeCollider = this.GetComponent<EdgeCollider2D>();
+    //    myLine = this.GetComponent<LineRenderer>();
+    //    myLine.startWidth = startWidth;
+    //    myLine.endWidth = endWidth;
+    //    UpdateColorAndHighlight();
+    //}
+
+
     protected override void UpdateColorAndHighlight()//update color based on hive and highlight
     {
-        
-        myLine.endColor = Target.GetComponent<SpriteRenderer>().color;
+        //SetLineColor();
+        SetLinkColor();
+    }
+
+    private void SetLinkColor()
+    {
         if (isHovered || isClicked)//highlighted
         {
-            //start color
-            if (Origin.HiveRef)
-                myLine.startColor = Origin.HiveRef.HiveHighlightColor;
-            else
-                myLine.startColor = ParamManager.Instance.NeutralHighlightColor;
-            //end color
-            if (Target.HiveRef)
-                myLine.endColor = Target.HiveRef.HiveHighlightColor;
-            else
-                myLine.endColor = ParamManager.Instance.NeutralHighlightColor;
+            _spriteRenderer.color = Origin.HiveRef.HiveHighlightColor;
         }
-        else//not highlithed
-        {
-            //start color
-            if (Origin.HiveRef)
-                myLine.startColor = Origin.HiveRef.HiveColor;
-            else
-                myLine.startColor = ParamManager.Instance.NeutralColor;
-            //end color
-            if (Target.HiveRef)
-                myLine.endColor = Target.HiveRef.HiveColor;
-            else
-                myLine.endColor = ParamManager.Instance.NeutralColor;
+        else {
+            _spriteRenderer.color = Origin.HiveRef.HiveColor;
         }
     }
+
+    //private void SetLineColor()
+    //{
+    //    myLine.endColor = Target.GetComponent<SpriteRenderer>().color;
+    //    if (isHovered || isClicked)//highlighted
+    //    {
+    //        //start color
+    //        if (Origin.HiveRef)
+    //            myLine.startColor = Origin.HiveRef.HiveHighlightColor;
+    //        else
+    //            myLine.startColor = ParamManager.Instance.NeutralHighlightColor;
+    //        //end color
+    //        if (Target.HiveRef)
+    //            myLine.endColor = Target.HiveRef.HiveHighlightColor;
+    //        else
+    //            myLine.endColor = ParamManager.Instance.NeutralHighlightColor;
+    //    }
+    //    else//not highlithed
+    //    {
+    //        //start color
+    //        if (Origin.HiveRef)
+    //            myLine.startColor = Origin.HiveRef.HiveColor;
+    //        else
+    //            myLine.startColor = ParamManager.Instance.NeutralColor;
+    //        //end color
+    //        if (Target.HiveRef)
+    //            myLine.endColor = Target.HiveRef.HiveColor;
+    //        else
+    //            myLine.endColor = ParamManager.Instance.NeutralColor;
+    //    }
+    //}
 
     public bool CompareExactTo(Link link)//is the same as another link
     {
